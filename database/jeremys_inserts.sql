b3097444afcbff871d6182b43d79850c18c2496e
bartojer <bartojer@gmail.com>
2025-03-26 18:48:50 -0600

    finish mesh table inserts

---
+++ database/jeremys_inserts.sql
@@ -0,0 +1,38 @@
+INSERT INTO mirage
+(mirage_build_out)
+VALUES
+('None'),
+('1" X 2" L Flap'),
+('5/8 Sq Tube'),
+('Mohair (back of housing)'),
+('Calk'),
+('Custom');
+
+-- CREATE TABLE IF NOT EXISTS mesh 
+-- (
+--   mesh_id SERIAL,
+--   mesh_type CHARACTER VARYING NOT NULL,
+--   CONSTRAINT mesh_pk PRIMARY KEY (mesh_id)
+-- );
+
+INSERT INTO mesh
+(mesh_type)
+VALUES
+("BV"),
+("Bug"),
+("Patio"),
+("Tuff"),
+("Pet"),
+("SS"),
+
+("Charcoal (18x14)"),
+("20x20 Insect Mesh"),
+("Solar Mesh (20x30)"),
+("Privacy Mesh SW2360"),
+("Privacy Mesh ES7505"),
+("Privacy Mesh ES7510"),
+("TuffScreen/NoSeeUm (Standard)"),
+("Suntex 80 Charcoal/Black (Non-Standard) (Add $100)"),
+("Suntex 90 Charcoal/Black (Non-Standard) (Add $100)"),
+("Suntex 95 Charcoal/Black (Non-Standard) (Add $100)"),
+("Custom (Non-Standard) (Add $??)");
