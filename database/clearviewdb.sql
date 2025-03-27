3a0dd6e56c0f2a7ab352254c22fb5437efb92056
Hayden Wilcox <hayden.grey.wilcox@gmail.com>
2025-03-26 18:42:35 -0600

    Product_mesh

--- database/clearviewdb.sql
+++ database/clearviewdb.sql
@@ -902,3 +902,28 @@ CREATE TABLE IF NOT EXISTS nws_measurement
   );
 
 
+
+
+-- INSERTS
+
+INSERT INTO color (color_name)
+VALUES ('Midnight Black', 'Slack Grey', 'Limen Cream', 'Rindeau Brown', 'Sandalwood', 'Tudor Brown', 'Polar White', 'Ex Pebble Grey'
+        , 'Hatford Green', 'Ansi 49 Grey', 'Almond', 'Autumn Leaf', 'Bergundy', 'Charcoal', 'Dk Bronze Metallic', 'Evergreen'
+        , 'Furniture White', 'Gloss Brown', 'Metro Brown', 'Mocha', 'Metalic Silver', 'Oyster Grey', 'Seal Beach Green', 'Sundried Tomato', 'Signal White', 'Wicker', 'Custom Match');
+
+INSERT INTO frame_size (size_type)
+VALUES ('1/4"', '5/16"', '3/8"', '7/16"', '1"', 'OC', 'IC');
+
+INSERT INTO fastener (fastener_type)
+VALUES ('None', 'RH (Ram Horn Spring)', 'PP (Pointed Plunger)', 'SP (Standard Plunger)', 'FP (Fat Plunger)', '1" FP (1" Fat Plunger)', 'PL (Pointer Latch)', 'CC (Cast Clip)', 'F-Chanel', 'H-Chanel', 'Combination', 'Custom')
+
+INSERT INTO mesh (mesh_type)
+VALUES ('BV', 'Bug', 'Patio', 'Tuff', 'Pet', 'SS')
+
+
+INSERT INTO PRODUCT (product_name)
+VALUES ("Mirage", "Mirage 3500", "New Window Screen", "Rainier")
+
+
+
+
